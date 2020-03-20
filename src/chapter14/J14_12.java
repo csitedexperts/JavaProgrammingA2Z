7f3802fb63121fa5e39a969838f9faaa3bce470c
CSITEd Experts <CSITEdExperts@gmail.com>
2020-03-20 11:50:48 -0400

    J14_12.java updated with a custom method added in the MyException class

--- src/chapter14/J14_12.java
+++ src/chapter14/J14_12.java
@@ -1,33 +1,39 @@
-package chapter14;
+package exceptionHandling;
 /*  J14_12.java   */
 /* Creating User-defined Exception Class */
 import java.lang.Exception;   // Optional Statement
-class MyException extends Exception
-{
- MyException(String Msg) // Constructor Defined
-  {
-   super(Msg); 
-  }
-}
 
- public class J14_12 {
- public static void main(String args[])
-  {
-  int x = 1, y = 100000;
-  float z;
-   try {
-    z = (float)x/(float)y ;
-    if(z<0.001)
-     throw new MyException("The result is too small");
-    } // End of Try Block
-   catch (MyException  exObj){ 
-// Catching user-defined Exception
-    System.out.println("Caught user-defined Exception");
-    System.out.println(exObj.getMessage() );
-   }  // End of Catch Block
-  finally
-  {
-   System.out.println("Program terminated successfully" );
-  }  // End of Default Block
- }
+class MyException extends Exception{
+	MyException(String Msg) // Constructor Defined
+	{ 	
+		super(Msg); 
+	}
+	public String myMessage() {
+		return "MyException::MyMessage() executed";
+
+	}
+
+}
+public class J14_12 {
+	public static void main(String args[]){
+		int x = 1, y = 100000;
+		float z;
+		try {
+			z = (float)x/(float)y ;
+			System.out.println("z = " + z);
+			if(z<0.001)
+				throw new MyException("The result is too small");
+		} // End of Try Block
+		catch (MyException  exObj){ 
+			// Catching user-defined Exception
+			System.out.println("Caught user-defined Exception");
+			System.out.println(exObj.getMessage() );
+			System.out.println(exObj.myMessage() );
+		}  // End of Catch Block
+
+		finally
+		{
+			System.out.println("Program terminated successfully" );
+		}  // End of Default Block
+	}
 }
